67133c6048ad30691f8a93a7eea6996eab6acce9
william <seongwilliam@gmail.com>
2026-04-08 17:44:48 -0400

    Initial commit, put rough, but working implementation of bb84 quantum key exhcnage simulation. Will branch and clean after

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Spr26-NetworkSecurityFinal-Quantum
+
+Implementation of 3 algorithms/protocols
+- Grover's Algorithm
+- BB84 Key Exchange
+- ML-KEM
+
+Final Project for Spring 2026 Network Security at CCNY
---
+++ bb84.py
@@ -0,0 +1,166 @@
+# This will be a simulation of BB84 QKE
+import random
+
+
+LENGTH = 1000
+BASIS = ["+", "x"]
+EVE = False
+
+
+def alice_random_bits(n: int) -> list[str]:
+    """
+        Alice will generate a random bit string of length n
+    """
+    print(f"Alice generating bit string of length {n}...")
+
+    rand_bits = [random.randint(0, 1) for _ in range(n)]
+
+    return rand_bits
+
+
+def alice_random_basis(n: int) -> list[str]:
+    """
+        Alice will generate random basis 
+    """
+    print(f"Alice generating basis list of length {n}...")
+
+    return rand_basis(n)
+
+
+def bob_random_basis(n: int) -> list[str]:
+    """
+        Bob will generate random basis 
+    """
+    print(f"Bob generating basis list of length {n}...")
+    
+    return rand_basis(n)
+
+
+def eve_
+
+
+def rand_basis(n: int) -> list[str]:
+    """
+        Helper function for calculating basis list
+    """
+    return [random.choice(BASIS) for _ in range(n)]
+
+
+def bob_measured_bits(bob_basis: list[str], alice_basis: list[str], alice_bits: list[str]) -> list[str]:
+    """
+        Bob will measure the qubits from Alice
+        To simulate Bob and Alice will compare basis lists
+
+        If they match Bob would have measured the bit correctly
+        else Bob will measure a random bit, 0 or 1
+    """
+    print("Bob measuring Alice's random qubits")
+
+    bob_bits = [alice_bits[i] if bob_basis[i] == alice_basis[i] else random.randint(0,1) for i in range(len(alice_bits))]
+    
+    return bob_bits
+
+
+def sift_keys(alice_basis: list[str],  bob_basis: list[str], alice_bits: list[str], bob_bits: list[str]):
+    """
+        Alice and Bob will compare basis lists in order to sift the key from the bit lists
+    """
+    alice_key_list = sift_keys_helper(alice_bits, alice_basis, bob_basis)
+    bob_key_list = sift_keys_helper(bob_bits, alice_basis, bob_basis)
+
+    return alice_key_list, bob_key_list
+
+
+def sift_keys_helper(bit_list: list[str], alice_basis: list[str], bob_basis: list[str]):
+    return [bit_list[i] for i in range(len(alice_basis)) if alice_basis[i] == bob_basis[i]]
+
+
+def error_check(alice_bits: list[str], bob_bits: list[str]):
+    """
+        Sample Alice and Bob's bits to check their error rate.
+        If above a certain threshold, likely eve observed the qubits before Bob
+    """
+
+    n = len(alice_bits)
+    sample_size = n // 2
+    sample_indices = set(random.sample(range(n), sample_size))
+    
+    error = 0
+    alice_final_key = []
+    bob_final_key = []
+
+    for i in range(len(alice_bits)):
+        if i in sample_indices:
+            if alice_bits[i] != bob_bits[i]:
+                error += 1
+        else:
+            alice_final_key.append(alice_bits[i])
+            bob_final_key.append(bob_bits[i])
+    
+    error_rate = error / sample_size
+
+    return alice_final_key, bob_final_key, error_rate
+
+
+def eve(n: int, alice_basis, alice_bits):
+    """
+        Eve will "observe" the bit list from Alice, corrupting the list
+    """
+
+    eve_basis = rand_basis(n)
+
+    print(f"eve basis: {eve_basis}")
+
+    print("Eve measuring Alice's random qubits")
+
+    eve_bits = [alice_bits[i] if eve_basis[i] == alice_basis[i] else random.randint(0,1) for i in range(len(alice_bits))]
+    
+    print(f"Eve bits: {eve_bits}")
+
+    return eve_basis, eve_bits
+
+
+
+def main():
+    # We will go through each process of the key exchange simulating Alice and Bob.
+    # We will also generate an Eve to see if Alice and Bob can correctly throw away a key that was observed by Eve
+
+    # Alice will generate a bits list of a certain length.
+    alice_rand_bit_list = alice_random_bits(LENGTH)
+    print(f"Alice produces random bit list: {alice_rand_bit_list}")
+
+    # Alice will also generate a basis list of the same length
+    alice_rand_basis_list = alice_random_basis(LENGTH)
+    print(f"Alice produces random basis list: {alice_rand_basis_list}")
+
+
+    # Eve will observe here
+    # Simulate if Eve observes
+    if EVE:
+        fake_alice_basis, fake_alice_bits = eve(LENGTH, alice_rand_basis_list, alice_rand_bit_list)
+    
+
+    # Bob will also generate a basis list
+    bob_rand_basis_list = bob_random_basis(LENGTH)
+    print(f"Bob produces random basis list: {bob_rand_basis_list}")
+
+    # Alice and Bob will compare their basis list, and Bob will "derive" a bit list
+    bob_bits = bob_measured_bits(bob_rand_basis_list, alice_rand_basis_list, alice_rand_bit_list)
+    print(f"Bob measures bits: {bob_bits}")
+    # In the event that Eve attacks
+    if EVE:
+        bob_bits = bob_measured_bits(bob_rand_basis_list, fake_alice_basis, fake_alice_bits)
+        print(f"Bob measures bits: {bob_bits}")
+
+
+    # Now the bit lists will be sifted based on the basis lists matches
+    alice_key, bob_key = sift_keys(alice_rand_basis_list, bob_rand_basis_list, alice_rand_bit_list, bob_bits)
+    print(f"Alice key is {alice_key} and Bob key is {bob_key}")
+
+    # Finally we will error check the actual bits publically
+    alice_final, bob_final, error_rate = error_check(alice_key, bob_key)
+    print(f"Alice Final: {alice_final} Bob Final: {bob_final} with error rate {error_rate}")
+
+
+if __name__ == "__main__":
+    main()
